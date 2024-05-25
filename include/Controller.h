bc45ae68b3637cc808fd1dbe11529057e3bdcace
Todo Lodo <me@todolodo.xyz>
2024-05-25 06:06:26 +0530

    added new functions and structs for controller code

--- include/Controller.h
+++ include/Controller.h
@@ -1,11 +1,19 @@
-//Example ATmega2560 Project
-//File: ATmega2560Project.h
-//Author: Robert Howie
+/**
+ * @file Controller.h
+ * @authors
+ * - Robot Howie
+ * - Todo Lodo
+ * @brief Controller file for MXEN2003 project
+ * @version 0.1
+ * @date 2024-05-23
+ *
+ * @copyright Copyright (c) 2024
+ */
 
-#ifndef CONTROLLER_H_ //double inclusion guard
+#ifndef CONTROLLER_H_ // double inclusion guard
 #define CONTROLLER_H_
 
-//include standard libraries
+// include standard libraries
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <stdbool.h>
@@ -13,19 +21,113 @@
 #include <util/delay.h>
 #include <stdio.h>
 
-//include header files
-#include "serial.h" //minimal serial lib
-#include "adc.h" //minimal adc lib
+// include header files
+#include "serial.h"		  //minimal serial lib
+#include "adc.h"		  //minimal adc lib
 #include "milliseconds.h" //milliseconds timekeeping lib
-#include "hd44780.h" //LCD lib
+#include "hd44780.h"	  //LCD lib
 
+// constants
+#define BUILD_DATE __TIME__ " " __DATE__ "\n"
 
-//constants
-#define BUILD_DATE __TIME__ " " __DATE__"\n"
+/**
+ * @defgroup ISRsController ISRs in Controller
+ * @brief Interrupt Service Routines in Controller.
+ */
 
-//declarations
-void timer_init();
-void computeMotorSpeeds(uint16_t motor_speed[2]);
-void computeCameraAngles(uint8_t camera_angle[2]);
+// declarations
+void button_init();
+
+/**
+ * @defgroup MotorFunctionsController Motor Functions in Controller
+ * @brief Functions for motor operations and data.
+ * @{
+ */
+
+/**
+ * @struct Motor
+ * @brief Contains data of each left and right motors
+ *
+ * @author Todo Lodo
+ */
+typedef struct
+{
+	/**
+	 * @var unsigned int l_val
+	 * @brief speed magnitude of left motor.
+	 */
+	unsigned int l_val : 9;
+
+	/**
+	 * @var unsigned int r_val
+	 * @brief speed magnitude of right motor.
+	 */
+	unsigned int r_val : 9;
+
+	/**
+	 * @var unsigned int l_dir
+	 * @brief direction indicator of left motor.
+	 */
+	unsigned int l_dir : 1;
+
+	/**
+	 * @var unsigned int r_dir
+	 * @brief direction indicator of left motor.
+	 */
+	unsigned int r_dir : 1;
+
+} Motor;
+
+/**
+ * @brief Compute each motor speed and direction.
+ *
+ * @param motor The memory address of the motor struct.
+ *
+ * @author Todo Lodo
+ */
+void computeMotorSpeeds(Motor *motor);
+
+/**
+ * @brief Compress motor struct data imto 20 bits.
+ *
+ * @param motor The memory address of the motor struct.
+ * @return uint32_t, compressed motor data.
+ *
+ * @author Todo Lodo
+ */
+uint32_t compressMotorData(Motor motor);
+
+/**
+ * @}
+ */
+
+/**
+ * @defgroup CamFunctionsController Camera Functions in Controller
+ * @brief Functions for camera operations and data.
+ * @{
+ */
+
+/**
+ * @brief Calculate camera angle corressponding to joystick input and previous angles.
+ *
+ * @param camera_angle camer angles array.
+ *
+ * @author Todo Lodo
+ */
+void computeCameraAngles(volatile uint8_t camera_angle[2]);
+
+/**
+ * @brief Compress camera data imto 16 bits.
+ *
+ * @param camera_angle camer angles array.
+ * @return uint8_t, compressed camera data.
+ *
+ * @author Todo Lodo
+ */
+uint16_t compressCameraData(volatile uint8_t camera_angle[2]);
+
+/**
+ * @}
+ */
 
 #endif /* ATMEGA2560_H_ */
