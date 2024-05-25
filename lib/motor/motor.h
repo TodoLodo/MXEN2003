7f21c4074fbedc7a222540a8cbf24f6f59c2eda5
Todo Lodo <me@todolodo.xyz>
2024-05-25 23:31:36 +0530

    added common Motor struct

---
+++ lib/motor/motor.h
@@ -0,0 +1,43 @@
+/**
+ * @defgroup MotorStruct Motor Struct
+ * @brief Motor Struct for both Controller and Motor.
+ * @{
+ */
+
+/**
+ * @struct Motor
+ * @brief Contains data of each left and right motors
+ *
+ * @author Todo Lodo
+ */
+typedef struct
+{
+	/**
+	 * @var unsigned int l_val
+	 * @brief speed magnitude of left motor.
+	 */
+	unsigned int l_val : 9;
+
+	/**
+	 * @var unsigned int r_val
+	 * @brief speed magnitude of right motor.
+	 */
+	unsigned int r_val : 9;
+
+	/**
+	 * @var unsigned int l_dir
+	 * @brief direction indicator of left motor.
+	 */
+	unsigned int l_dir : 1;
+
+	/**
+	 * @var unsigned int r_dir
+	 * @brief direction indicator of left motor.
+	 */
+	unsigned int r_dir : 1;
+
+} Motor;
+
+/**
+ * @}
+ */
